9ee73fe3e43c688a69506672ba5299f860695144
Dave20220816 <aponde@163.com>
2022-08-18 06:05:21 +0800

    test branch modify3

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -3,5 +3,8 @@ package com.duyi;
 public class MainTest {
     public static void main(String[]args){
         System.out.println("hellow git");
+
+        System.out.println("hellow test");
+
     }
 }
