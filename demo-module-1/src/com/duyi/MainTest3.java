d53bf6f8723745d18cfd0341f8329ea673e5a580
Dave20220816 <aponde@163.com>
2022-08-18 06:18:06 +0800

    master branch modify5

--- demo-module-1/src/com/duyi/MainTest3.java
+++ demo-module-1/src/com/duyi/MainTest3.java
@@ -1,4 +1,11 @@
 package com.duyi;
 
 public class MainTest3 {
+    public static void main(String[]args){
+        System.out.println("hellow git");
+
+        System.out.println("hellow master");
+
+        System.out.println("test change");
+    }
 }
