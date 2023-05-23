20cad8523c4f8e917089d0c4f749a090289c5516
Dave20220816 <aponde@163.com>
2023-05-23 11:17:06 +0800

    2023 year TestStream.java modify

--- demo-module-8/src/com/bili/TestStream.java
+++ demo-module-8/src/com/bili/TestStream.java
@@ -1,4 +1,16 @@
 package com.bili;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TestStream {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fileInputStream=new FileInputStream("D:/play/ggg/test.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
